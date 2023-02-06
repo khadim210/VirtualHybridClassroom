23fca78cd36518028a4862204e30b3a142165b92
MBACKE <ahmathbamba.mbacke@gmail.com>
2023-02-06 22:26:44 +0000

    Update Install_VirtualClassConnector.sh

--- Install_VirtualClassConnector.sh
+++ Install_VirtualClassConnector.sh
@@ -3,7 +3,7 @@ sudo echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/s
 sudo apt-get update
 
 echo "2- Dependancies intallation..."
-sudo apt-get install git wireguard* resolvconf bridge-utils
+sudo apt-get install git wireguard* resolvconf bridge-utils net-tools
 
 echo "3- Scripts download..."
 git clone https://github.com/khadim210/VirtualHybridClassroom /usr/bin/VirtualHybridClassroom
